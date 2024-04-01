a1295e5bcf42f632d309a5e66df6f744e2727550
rose <rose.tj44@gmail.com>
2024-04-01 21:57:39 +0530

    Added Packages details

---
+++ hospitality/hospitality_operations/doctype/manage_packages/manage_packages.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, service and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Manage Packages", {
+// 	refresh(frm) {
+
+// 	},
+// });
